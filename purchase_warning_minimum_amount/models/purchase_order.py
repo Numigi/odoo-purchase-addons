4be24b4f1803de8a7444f91d9213b29efbb610af
kazacube-mziouadi <mziouadi@kazacube.com>
2019-08-17 14:49:31 +0100

    update code : refactore code display wizard warning

--- purchase_warning_minimum_amount/models/purchase_order.py
+++ purchase_warning_minimum_amount/models/purchase_order.py
@@ -10,27 +10,30 @@ class PurchaseOrder(models.Model):
 
     min_purchase_amount = fields.Float(string="Minimum Purchase amount", related='partner_id.min_purchase_amount')
 
+    def display_alert_wizard(self, context):
+        view = self.env.ref('purchase_warning_minimum_amount.wizard_alert_message')
+        wiz = self.env['wizard.alert'].create({'order_id': self.id})
+        return {
+            'name': 'Warning',
+            'type': 'ir.actions.act_window',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'wizard.alert',
+            'views': [(view.id, 'form')],
+            'view_id': view.id,
+            'target': 'new',
+            'res_id': wiz.id,
+            'context': context,
+        }
+
     @api.multi
     def button_confirm(self):
         context = self._context.copy()
         if context.get('confirm_order', False):
             return super(PurchaseOrder, self).button_confirm()
         if self.amount_untaxed < self.min_purchase_amount:
-            view = self.env.ref('purchase_warning_minimum_amount.wizard_alert_message')
-            wiz = self.env['wizard.alert'].create({'order_id': self.id})
             context.update({'to_confirm': True})
-            return {
-                'name': 'Warning',
-                'type': 'ir.actions.act_window',
-                'view_type': 'form',
-                'view_mode': 'form',
-                'res_model': 'wizard.alert',
-                'views': [(view.id, 'form')],
-                'view_id': view.id,
-                'target': 'new',
-                'res_id': wiz.id,
-                'context': context,
-            }
+            return self.display_alert_wizard(context)
         return super(PurchaseOrder, self).button_confirm()
 
     @api.multi
@@ -39,19 +42,6 @@ class PurchaseOrder(models.Model):
         if context.get('confirm_order', False):
             return super(PurchaseOrder, self).button_approve()
         if self.amount_untaxed < self.min_purchase_amount:
-            view = self.env.ref('purchase_warning_minimum_amount.wizard_alert_message')
-            wiz = self.env['wizard.alert'].create({'order_id': self.id})
             context.update({'to_approve': True})
-            return {
-                'name': 'Warning',
-                'type': 'ir.actions.act_window',
-                'view_type': 'form',
-                'view_mode': 'form',
-                'res_model': 'wizard.alert',
-                'views': [(view.id, 'form')],
-                'view_id': view.id,
-                'target': 'new',
-                'res_id': wiz.id,
-                'context': context,
-            }
+            return self.display_alert_wizard(context)
         return super(PurchaseOrder, self).button_approve()
