d33aaa85be4b419bdf0f88f7b3ba4c37d361c9d4
David Dufresne <27902736+ddufresne@users.noreply.github.com>
2020-04-16 14:13:25 -0400

    Add filters to view pickings invoiced and partially invoiced

--- purchase_invoice_from_picking/models/stock_move.py
+++ purchase_invoice_from_picking/models/stock_move.py
@@ -1,13 +1,25 @@
 # © 2020 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class StockMove(models.Model):
 
-    _inherit = 'stock.move'
+    _inherit = "stock.move"
 
     supplier_invoice_line_ids = fields.One2many(
-        'account.invoice.line', 'receipt_move_id',
+        "account.invoice.line", "receipt_move_id"
     )
+
+    receipt_invoiced = fields.Boolean(compute="_compute_receipt_invoiced", store=True)
+
+    @api.depends(
+        "supplier_invoice_line_ids", "supplier_invoice_line_ids.invoice_id.state"
+    )
+    def _compute_receipt_invoiced(self):
+        for move in self:
+            non_cancelled_invoice_lines = move.supplier_invoice_line_ids.filtered(
+                lambda l: l.invoice_id.state != "cancel"
+            )
+            move.receipt_invoiced = bool(non_cancelled_invoice_lines)
--- purchase_invoice_from_picking/models/stock_picking.py
+++ purchase_invoice_from_picking/models/stock_picking.py
@@ -1,11 +1,29 @@
 # © 2019 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class StockPicking(models.Model):
 
     _inherit = "stock.picking"
 
-    supplier_invoice_ids = fields.One2many("account.invoice", "receipt_picking_id")
+    receipt_invoiced = fields.Boolean(compute="_compute_receipt_invoiced", store=True)
+    receipt_partially_invoiced = fields.Boolean(
+        compute="_compute_receipt_invoiced", store=True
+    )
+
+    @api.depends("move_lines.receipt_invoiced")
+    def _compute_receipt_invoiced(self):
+        for picking in self:
+            invoiced_moves = picking._get_invoiced_moves()
+            uninvoiced_moves = picking.move_lines - invoiced_moves
+            picking.receipt_invoiced = (
+                True if invoiced_moves and not uninvoiced_moves else False
+            )
+            picking.receipt_partially_invoiced = (
+                True if invoiced_moves and uninvoiced_moves else False
+            )
+
+    def _get_invoiced_moves(self):
+        return self.move_lines.filtered(lambda m: m.receipt_invoiced)
