403953b87f50ce1f3b46826b8e9e13df881fad46
Lanto Razafindrabe <lanto.razafindrabe@numigi.com>
2023-03-03 08:20:09 +0300

    TA#50017 [ADD] purchase_sale_inter_company_description_line: copy description from PO line to SO line

--- .docker_files/main/__manifest__.py
+++ .docker_files/main/__manifest__.py
@@ -29,6 +29,7 @@
         "stock_orderpoint_editable_list",
         "stock_picking_groupby_purchase_user",
         "stock_picking_supplier_reference",
+        "purchase_sale_inter_company_description_line",
     ],
     "installable": True,
 }
