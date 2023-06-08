04f334d9659b714909011b83faeb1e38c5c476f7
Majda EL MARIOULI <majdaelmariouli@gmail.com>
2023-06-08 22:14:17 +0100

    Fix Quality Code

--- service_to_purchase_multico/models/sale_order.py
+++ service_to_purchase_multico/models/sale_order.py
@@ -1,10 +1,6 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-import logging
+# © 2023 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
 
 from odoo import api, models
-
-_logger = logging.getLogger(__name__)
 
 
 class SaleOrderLine(models.Model):
