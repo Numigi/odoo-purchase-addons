f0d0b176bd77d7305ebec88bf4fa4e8ea088ce2b
Majda EL MARIOULI <majdaelmariouli@gmail.com>
2023-06-08 22:12:41 +0100

    Fix Quality Code

--- service_to_purchase_multico/models/sale_order.py
+++ service_to_purchase_multico/models/sale_order.py
@@ -13,10 +13,10 @@ class SaleOrderLine(models.Model):
     @api.multi
     def _purchase_service_generation(self):
         company_id = self.mapped("company_id")
-        if company_id: 
+        if company_id:
             res = super(
                 SaleOrderLine, self.with_context(force_company=company_id[0].id)
             )._purchase_service_generation()
-        else: 
+        else:
             res = super(SaleOrderLine, self)._purchase_service_generation()
         return res
