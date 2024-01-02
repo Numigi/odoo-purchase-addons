565a8582c1b4386cdbec16b7fb9b4be5845d46c9
Majda EL MARIOULI <majdaelmariouli@gmail.com>
2024-01-02 09:35:06 -0500

    TA#48502 [IMP] Clean Repository (#57)

--- .unported/purchase_warning_minimum_amount/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# © 2019 Numigi (tm) and all its contributors (https://bit.ly/numigiens)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0).
-
-from . import res_partner
-from . import purchase_order
