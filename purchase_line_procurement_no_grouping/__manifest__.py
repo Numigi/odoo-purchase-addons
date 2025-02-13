7e8bcd5e1c2a7cdc2df7b5c61c301c6dc78625a5
majouda <majdaelmariouli@gmail.com>
2025-02-12 22:21:09 -0500

    TA#73500 [14.0][ADD] purchase_line_procurement_no_grouping

---
+++ purchase_line_procurement_no_grouping/__manifest__.py
@@ -0,0 +1,20 @@
+# © 2025 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Purchase Line Procurement No Grouping",
+    "version": "14.0.1.0.0",
+    "author": "Numigi",
+    "maintainer": "Numigi",
+    "website": "https://bit.ly/numigi-com",
+    "license": "LGPL-3",
+    "category": "Purchase",
+    "summary": "Prevent grouping purchase order lines with different progurement group",
+    "depends": [
+        "purchase_stock",
+    ],
+    "data": [
+        "views/purchase_order_views.xml",
+    ],
+    "installable": True,
+}
---
+++ purchase_line_procurement_no_grouping/models/purchase_order_line.py
@@ -0,0 +1,48 @@
+# Copyright 2025 - today Numigi (tm) and all its contributors (https://bit.ly/numigiens)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import api, fields, models
+
+
+class PurchaseOrderLine(models.Model):
+    _inherit = "purchase.order.line"
+
+    group_id = fields.Many2one(
+        "procurement.group", string="Procurement Group", index=True, copy=False
+    )
+
+    @api.model
+    def _prepare_purchase_order_line_from_procurement(
+        self, product_id, product_qty, product_uom, company_id, values, po
+    ):
+        vals = super()._prepare_purchase_order_line_from_procurement(
+            product_id, product_qty, product_uom, company_id, values, po
+        )
+        if "group_id" in values and values["group_id"]:
+            vals["group_id"] = values["group_id"].id
+        return vals
+
+    def _find_candidate(
+        self,
+        product_id,
+        product_qty,
+        product_uom,
+        location_id,
+        name,
+        origin,
+        company_id,
+        values,
+    ):
+        """We filter by procurement group to prevent grouping by line.
+        """
+        self = self.filtered(lambda x: x.group_id == values.get("group_id"))
+        return super(PurchaseOrderLine, self)._find_candidate(
+            product_id,
+            product_qty,
+            product_uom,
+            location_id,
+            name,
+            origin,
+            company_id,
+            values,
+        )
