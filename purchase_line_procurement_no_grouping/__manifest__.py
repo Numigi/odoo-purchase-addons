c618ec84ce9c9097027061d05f562b24c7f836a4
Abdellatif BENZBIRIA <a.benzbiria@magnetomedia.net>
2025-02-25 03:32:43 +0000

    [IMP] purchase_line_procurement_no_grouping : rename Procurement Group (#84)

--- purchase_line_procurement_no_grouping/__manifest__.py
+++ purchase_line_procurement_no_grouping/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Purchase Line Procurement No Grouping",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.0.1",
     "author": "Numigi",
     "maintainer": "Numigi",
     "website": "https://bit.ly/numigi-com",
